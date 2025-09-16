ed9a7275475c91665836a7e7b0389590868b83d8
mahita23-hub <mahitapatil@gmail.com>
2025-09-16 20:48:53 +0530

    This is Inheritance demo it also even contains activities

---
+++ inheritance/src/inheritance/Demo.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class Demo {
+	    public static void main(String[] args) {
+	        Level1Employee emp = new Level1Employee("Ravi", 28, "E102", "IT", "Level-1");
+	        System.out.println(emp);
+	    }
+	}
